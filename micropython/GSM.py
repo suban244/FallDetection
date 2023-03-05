e96ff110ab431e9904154ea42a01337d36fad3f5
Suban <076bct082.suban@pcampus.edu.np>
2023-03-05 23:01:25 +0545

    micropython port on its way

---
+++ micropython/GSM.py
@@ -0,0 +1,41 @@
+from machine import UART
+import time
+
+endingchar = bytes()
+endingchar += chr(26)
+txData = b'AT\r\n'
+
+
+def sendSMS(uart: UART, number: str):
+    while uart.any():
+        uart.read(1)
+    time.sleep(0.1)
+
+    words = f'AT+CMGS=\"+977{number}\"\r\n'
+
+    uart.write(b"AT+CMGF=1\r\n")
+    time.sleep(1)
+    uart.write(words.encode('utf-8'))
+    time.sleep(1)
+    uart.write(b'hello from micropython\nBye')
+    time.sleep(0.1)
+    uart.write(endingchar)
+    time.sleep(1)
+
+    rxData = bytes()
+    while uart.any() > 0:
+        rxData += uart.read(1)
+
+    print(rxData.decode('utf-8'))
+    time.sleep(1)
+
+
+def checkStatus(uart: UART):
+    uart.write(txData)
+    time.sleep(1)
+    rxData = bytes()
+
+    while uart.any() > 0:
+        rxData += uart.read(1)
+
+    print(len(rxData), rxData.decode('utf-8'))
