67de5c6c3067923b2f1e28b4d5d7516a5aa01453
Yunika-Bajracharya <076bct095.yunika@pcampus.edu.np>
2023-03-09 14:14:31 +0545

    some more data

--- data/collect.sh
+++ data/collect.sh
@@ -1,3 +1,5 @@
-sleep 5
+# sleep 5
 echo "starting"
-python read.py "fallback1.csv"
+# python read.py "fall_detection_dataset/fall/fall_rightside/fallrightside0.csv"
+# python read.py "fall_detection_dataset/ADL/upstairs/upstairs0.csv"
+python read.py "fall_detection_dataset/ADL/yu/.csv"
