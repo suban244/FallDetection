23ef5ce341b22a42b4fb43a2436b0f511f88bf93
Suban <076bct082.suban@pcampus.edu.np>
2023-03-24 17:43:01 +0545

    datacollect

--- split.py
+++ split.py
@@ -1,27 +1,22 @@
-import csv
-import pandas as pd
-import random
 import numpy as np
+import random
+import pandas as pd
+import csv
 
-# Provide file name with path for example: "C:\Users\xxxxx\flights.csv"
-split_source_file = "downstairs.csv"
-
-# find number of lines using Pandas
-df = pd.read_csv("downstairs0.csv")
+filename = 'walkSaurav1'
+filepath = 'data/'+filename+'.csv'
+df = pd.read_csv(filepath)
 
 input_seq_length = 30
-
 df_len = df.shape[0]
-print(df_len)
-print(df_len-input_seq_length)
 
-starting_index = np.random.randint(0, high=df_len-input_seq_length)
+for i in range(30):
+    starting_index = np.random.randint(0, high=df_len-input_seq_length)
+    df2 = df.iloc[starting_index: starting_index + input_seq_length]
 
-df2 = df.iloc[starting_index: starting_index + input_seq_length]
+    # open the file in the write mode
+    f = open(f'split/{filename}_{i}.csv', 'w')
 
-# open the file in the write mode
-f = open(f'1.csv', 'w')
+    df2.to_csv(f)
 
-df2.to_csv(f)
-
-f.close()
+    f.close()
